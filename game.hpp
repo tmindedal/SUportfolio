0c04c2e5d119dae060b396619d99fe8fd74d78d5
tmindedal <tmindedal@gmail.com>
2025-04-22 18:22:24 +0200

    Hero Class

--- game.hpp
+++ game.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 
-class character {
+class Character {
     
     protected:
         std::string name;    
@@ -12,17 +12,35 @@ class character {
         int strength;
     
     public:
-        Character()(std::string name, int health, int strength);
+        Character(std::string name, int health, int strength);
         virtual ~Character() = default;
 
         std::string getName() const;
         int getHealth() const;
         int getStrength() const;
+       
         void damage(int amount);
         bool isAlive() const;
 };
 
 
+class Hero : public Character {
+    
+    private:
+        int level;
+        int xp;
+        int xpToLevelUp;
+    
+    public:
+        Hero(std::string name, int health, int strength, int level);
+        
+        int getXp() const;
+        int getLevel() const;
+        
+        void gainXp(int amount);
+        void levelUp();
+
+};
 
 
 
