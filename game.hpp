81c096311ef5498ebb4c0b1790096ff814d15367
tmindedal <tmindedal@gmail.com>
2025-04-22 18:06:13 +0200

    tryout

---
+++ game.hpp
@@ -0,0 +1,21 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include <string>
+
+
+class character {
+    protected:
+        int hp;
+        int strength;
+        std::string name;
+    
+    public:
+        character() : 
+};
+
+
+
+
+
+#endif // GAME_HPP
