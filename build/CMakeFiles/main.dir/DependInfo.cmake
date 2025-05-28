59f2ef6a62b15abb8ed89275f1d2f4c2e7ab5db0
tmindedal <tmindedal@gmail.com>
2025-05-28 15:59:14 +0200

    Making Ubuntu 22.04 compatible

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/thomasm/SUportfolio/sqlite3.c" "CMakeFiles/main.dir/sqlite3.c.o" "gcc" "CMakeFiles/main.dir/sqlite3.c.o.d"
+  "/home/thomasm/SUportfolio/game.cpp" "CMakeFiles/main.dir/game.cpp.o" "gcc" "CMakeFiles/main.dir/game.cpp.o.d"
+  "/home/thomasm/SUportfolio/gameMain.cpp" "CMakeFiles/main.dir/gameMain.cpp.o" "gcc" "CMakeFiles/main.dir/gameMain.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/game.cpp.o"
+  "CMakeFiles/main.dir/game.cpp.o.d"
+  "CMakeFiles/main.dir/gameMain.cpp.o"
+  "CMakeFiles/main.dir/gameMain.cpp.o.d"
+  "CMakeFiles/main.dir/sqlite3.c.o"
+  "CMakeFiles/main.dir/sqlite3.c.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
